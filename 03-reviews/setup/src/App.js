b8d021191d58bd0da8b8de34427b4056999bef75
jesus <jesus.alberto@desici.com>
2021-10-12 13:07:53 -0500

    03- Reviews react icons

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Review from './Review';
+import {FaGithub} from "react-icons/fa";
 function App() {
-  return <h2>reviews project setup</h2>;
+  return <h2>reviews project setup <FaGithub className="icon" /></h2>;
 }
 
 export default App;
