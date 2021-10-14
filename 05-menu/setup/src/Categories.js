cf9f22f228a7de53a0116adffd2a89a732741f77
jesus <jesusalbertoramirezlopez@gmail.com>
2021-10-13 21:05:53 -0500

    05- Menu categories dynamic approach

--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-const Categories = ({filterItems}) => {
+const Categories = ({categories, filterItems}) => {
   return <div className="btn-container">
-    <button className="filter-btn" onClick={()=>filterItems('breakfast')}>
-      breakfast
-    </button>
+    { categories.map((category, index)=>{
+      return <button key={category, index} className="filter-btn" onClick={()=>filterItems(category)}>
+        {category}
+      </button>
+    }) }
   </div>;
 };
 
