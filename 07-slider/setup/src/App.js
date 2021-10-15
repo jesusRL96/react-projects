a7cb5194f0887f032b598d664b226009cc93b6e9
jesus <jesusalbertoramirezlopez@gmail.com>
2021-10-14 21:12:11 -0500

    07-Slider first, last, autoslide

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -4,7 +4,27 @@ import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
   const [people, setPeople] = useState(data)
-  const [index, setIndex] = useState(0)
+  const [index, setIndex] = useState(0) 
+
+  useEffect(() => {
+    const lastIndext = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndext)
+    }
+
+    if (index > lastIndext) {
+      setIndex(0)
+    }
+    
+  }, [index, people]);
+
+  useEffect(()=>{
+    let slide = setInterval(() => {
+      setIndex(index + 1);
+    }, 3000);
+    return () => clearInterval(slide);
+  },[index]);
+
   return (
     <section className="section">
       <div className="title">
