0da6a4eb5fb30b71fb4b93c0a56918e19f59d96b
jesus <jesusalbertoramirezlopez@gmail.com>
2021-10-20 21:15:45 -0500

    10-grocery-bud local storage

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -2,9 +2,19 @@ import React, { useState, useEffect } from 'react'
 import List from './List'
 import Alert from './Alert'
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
+  if (list) {
+    return JSON.parse(list)
+  }
+  else{
+    return []
+  }
+}
+
 function App() {
   const [name, setName] = useState('');
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage());
   const [isEditing, setIsEditing] = useState(false); 
   const [editID, setEditID] = useState(null); 
   const [alert, setAlert] = useState({show:false, msg:'', type:''});
@@ -57,6 +67,10 @@ function App() {
   const showAlert = (show=false,type='',msg='') => {
     setAlert({show,type,msg})
   }
+
+  useEffect(() => {
+    localStorage.setItem('list',JSON.stringify(list))
+  },[list]);
 
 
   return (
