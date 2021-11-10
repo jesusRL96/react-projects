729b9cb1eecb838e0f0a4bc95f3d67b165f1a1a0
jesus <jesusalbertoramirezlopez@gmail.com>
2021-11-09 21:01:09 -0600

    12-Sidebar complete

--- 12-sidebar-modal/setup/src/Home.js
+++ 12-sidebar-modal/setup/src/Home.js
@@ -4,15 +4,14 @@ import {AppContext, useGlobalContext} from './context'
 
 
 const Home = () => {
-  const data = useGlobalContext();
-  console.log(data);
+  const {openSidebar, openModal} = useGlobalContext();
   return (
     <main>
-      <button className="sidebar-toggle">
+      <button className="sidebar-toggle" onClick={openSidebar}>
         <FaBars />
       </button>
   
-      <button className="btn">
+      <button className="btn" onClick={openModal}>
         show modal
       </button>
     </main>
