9c524ed107f861cff27833e2b5e76017ad0e18d5
JesusRL96 <jesusalbertoramirezlopez@gmail.com>
2021-12-06 20:35:17 -0600

    13 Stripe context bp

--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,39 @@
 import React, { useState, useContext } from 'react'
 import sublinks from './data'
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({children}) => {
+    const [isSidebarOpened, setSidebarOpened] = useState(true);
+    const [isSubmenuOpened, setSubmenuOpened] = useState(true);
+
+    const openSidebar = () => {
+        setSidebarOpened(true);
+    };
+    const closeSidebar = () => {
+        setSidebarOpened(false);
+    };
+    
+    const openSubmenu = () => {
+        setSubmenuOpened(true);
+    };
+    const closeSubmenu = () => {
+        setSubmenuOpened(false);
+    };
+
+    return <AppContext.Provider value={{
+        isSubmenuOpened, 
+        isSidebarOpened, 
+        openSubmenu, 
+        openSidebar, 
+        closeSubmenu, 
+        closeSidebar,
+        }}
+        >
+            {children}
+        </AppContext.Provider>
+}
+
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+}
