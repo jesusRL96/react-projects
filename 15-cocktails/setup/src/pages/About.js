8addc5a6d56300dc193336d158526aebd51ba713
JesusRL96 <jesusalbertoramirezlopez@gmail.com>
2022-01-14 14:24:26 -0600

    15-Cocktails Router Error/About

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta delectus accusamus dolor, maxime perspiciatis error asperiores vel. Tempore, minima neque nam, facere ab deserunt aperiam odit asperiores, qui quisquam dolorum reprehenderit necessitatibus impedit sapiente repudiandae eum est. Non, voluptas repellat.</p>
+    </section>
   )
 }
 
