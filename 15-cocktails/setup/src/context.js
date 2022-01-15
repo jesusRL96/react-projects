aa8f4576bdc44e86dafeda83f35f29002e272de5
JesusRL96 <jesusalbertoramirezlopez@gmail.com>
2022-01-15 14:37:02 -0600

    15-Cocktails Single cocktail page

--- 15-cocktails/setup/src/context.js
+++ 15-cocktails/setup/src/context.js
@@ -9,32 +9,32 @@ const AppProvider = ({ children }) => {
   const [searchTerm, setSearchTerm] = useState('');
   const [cocktails, setCocktails] = useState([]);
 
-  const fetchDrinks = async () => {
-    setLoading(true);
-    try {
-      const response = await fetch(`${url}${searchTerm}`);
-      const data = await response.json();
-      const {drinks} = data;
-      if (drinks) {
-        const newDrinks = drinks.map((item)=>{
-          const {idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass} = item;
-          return {id:idDrink, name:strDrink, image:strDrinkThumb, info:strAlcoholic, glass:strGlass}
-        });
-        setCocktails(newDrinks);
+  const fetchDrinks = useCallback(async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(`${url}${searchTerm}`);
+        const data = await response.json();
+        const {drinks} = data;
+        if (drinks) {
+          const newDrinks = drinks.map((item)=>{
+            const {idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass} = item;
+            return {id:idDrink, name:strDrink, image:strDrinkThumb, info:strAlcoholic, glass:strGlass}
+          });
+          setCocktails(newDrinks);
+        }
+        else {
+          setCocktails([]);
+        }
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
       }
-      else {
-        setCocktails([]);
-      }
-      setLoading(false);
-    } catch (error) {
-      console.log(error);
-      setLoading(false);
-    }
-  };
+    }, [searchTerm]);
 
   useEffect(()=>{
     fetchDrinks();
-  },[searchTerm]);
+  },[searchTerm, fetchDrinks]);
   
   return <AppContext.Provider value={{
     loading,
